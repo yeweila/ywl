2a73deaa094d4df8f19f1c63fff31f01943f9ed3
ywl <3266223120@qq.com>
2019-07-21 20:02:56 +0800

    这是第一次提交

---
+++ blog/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+
+
+# Create your views here.
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{}({}) recommends you reading"{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title,
+                                                                    post_url,
+                                                                    cd['name'],
+                                                                    cd['comments'])
+            send_mail(subject, message, '1275095696@qq.com', [cd['to'], ])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/share.html', {'post': post, 'form': form})
+
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/list.html', {"page": page, "posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/detail.html', {"post": post,
+                                                "comments": comments,
+                                                "new_comment": new_comment,
+                                                "comment_form": comment_form})
